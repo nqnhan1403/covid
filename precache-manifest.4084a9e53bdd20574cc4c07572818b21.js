bf24e1817fb2f4145efd79806619b8ed7287ff91
Nhan Nguyen <nnguyen220@dxc.com>
2022-11-17 15:11:09 +0700

    Updates

---
+++ precache-manifest.4084a9e53bdd20574cc4c07572818b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b5ef3a384a3b33084e180a53145074",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "d94157ec1138cafdc362",
+    "url": "/covid/static/css/main.1a6a07f6.chunk.css"
+  },
+  {
+    "revision": "f3f4aab38685cdcf6f4c",
+    "url": "/covid/static/js/2.6652bd66.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.6652bd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94157ec1138cafdc362",
+    "url": "/covid/static/js/main.2ac9ec6a.chunk.js"
+  },
+  {
+    "revision": "dd7f6c094f0f5e36b6b3",
+    "url": "/covid/static/js/runtime-main.ed7b7e5f.js"
+  }
+]);
